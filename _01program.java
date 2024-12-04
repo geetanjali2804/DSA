7093969232d4064eb07ebb331cd20995fe2ee366
geetanjali2804 <156069449+geetanjali2804@users.noreply.github.com>
2024-12-04 19:16:29 +0530

    2nd commit

---
+++ _01program.java
@@ -0,0 +1,11 @@
+//traversing in array
+package BASICS;
+class _01program{
+    public static void main(String[]args){
+        int [] arr = {10,20,30,40,50};
+        //traversing each element of an array
+        for(int i =0;i < arr.length;i++){
+            System.out.println("Element at index " + i + " is " + arr[i]);
+        }
+    }
+}
